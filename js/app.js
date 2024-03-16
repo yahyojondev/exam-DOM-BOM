294e692532f0a1ba4e7cc8ee34bb888e3a8c8dfb
yahyojondev <nabijonovdeveloper@gmail.com>
2024-03-16 12:11:46 +0500

    hello

---
+++ js/app.js
@@ -0,0 +1,40 @@
+const API_URL = "https://dummyjson.com/products"
+const cardwrapper = document.querySelector(".cards__wrapper")
+const loading = document.querySelector(".loading")
+
+async function fetchData(api){
+    let data = await fetch(api)
+    data
+    .json()
+    .then(res=>createCard(res.products))
+    .catch(err=>console.log(err))
+    .finally(()=>{
+        loading.style.display="none"
+    })
+}
+fetchData(API_URL)
+
+function createCard(data){
+    data.forEach(products=>{
+        let card = document.createElement("div")
+        card.classList.add("cards__wrapper__card")
+        let cardContent = document.createElement("div")
+        cardContent.classList.add("card__content")
+        card.innerHTML = `
+        <img src=${products.images[0]} alt="">
+        
+                         
+        `
+        cardContent.innerHTML = `
+                 <h2 class="products__title">${products.title}</h2>
+         <div class="products__wrapper">
+                 <img class="products__rating" src="./images/main/stars.png" alt="">
+                <span class="products__stock">(${products.stock}) reviews</span>
+            </div>
+        <p class="products__text">${products.description}</p>
+                     <p class="products__price">${products.price}₽</p>  
+        `
+        cardwrapper.appendChild(card)
+        card.appendChild(cardContent)
+    })
+}
